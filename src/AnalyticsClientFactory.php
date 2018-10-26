3a4e1d15e2eb6a217d3424831900e498ee91ab49
iMestav <ibrahimmestav@gmail.com>
2018-10-26 20:32:42 +0300

    Update AnalyticsClientFactory.php

--- src/AnalyticsClientFactory.php
+++ src/AnalyticsClientFactory.php
@@ -23,16 +23,30 @@ class AnalyticsClientFactory
     public static function createAuthenticatedGoogleClient(array $config): Google_Client
     {
         $client = \Google::getClient();
-        $client->setAccessToken($config['access_token']);
-        if($config['expires']){
-            $newCredentials = $client->refreshToken($config['access_token']);
-            User::find($config['startup_id'])->apiConnects()->whereProvider('google')->first()->update([
-                'access_token' => $newCredentials['access_token'],
-                'refresh_token' => $newCredentials['refresh_token'],
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'expires_in' => Carbon::now()->addSeconds($newCredentials['expires_in'])->format('Y-m-d H:i:s')
-            ]);
+        $client->setAccessType("offline");
+        $client->setAccessToken($config['token']);
+
+        if ($client->isAccessTokenExpired()) {
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                $authUrl = $client->createAuthUrl();
+                throw new \Exception($authUrl);
+            }
         }
+
+        // if($config['expires']) {
+        //     $newCredentials = $client->refreshToken($config['access_token']);
+        //     dd($newCredentials);
+        //     if($newCredentials != null || $newCredentials != []) {
+        //         User::find($config['startup_id'])->apiConnects()->whereProvider('google')->first()->update([
+        //             'access_token' => $newCredentials['access_token'],
+        //             'refresh_token' => $newCredentials['refresh_token'],
+        //             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //             'expires_in' => Carbon::now()->addSeconds($newCredentials['expires_in'])->format('Y-m-d H:i:s')
+        //         ]);
+        //     }
+        // }
         return $client;
     }
 
